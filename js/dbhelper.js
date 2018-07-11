3eb21edba03073b68aa9f9615fbbeb071e768c6e
Paulina Walawko <paulina.walawko@stxnext.pl>
2018-07-11 21:05:45 +0200

    added token in the restaurant_info

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -18,8 +18,6 @@ class DBHelper {
   static fetchRestaurants(callback) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.setRequestHeader( 'Access-Control-Allow-Origin', '*');
-    xhr.setRequestHeader( 'Content-Type', 'application/json' );
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
@@ -159,7 +157,7 @@ class DBHelper {
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
       alt: restaurant.name,
@@ -167,7 +165,7 @@ class DBHelper {
       })
       marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
